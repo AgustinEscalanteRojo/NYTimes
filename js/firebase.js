876aa790898c2595119ec108a802888282d94e52
Agustin <agustinescalanterojo@gmail.com>
2023-05-02 19:39:13 +0200

    initial commit

---
+++ js/firebase.js
@@ -0,0 +1,31 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.20.0/firebase-app.js';
+import { getAuth } from 'https://www.gstatic.com/firebasejs/9.20.0/firebase-auth.js'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBr6aDt0Yvaqa4whswWBpfThSb257nVEpA",
+  authDomain: "nytimes-dd655.firebaseapp.com",
+  projectId: "nytimes-dd655",
+  storageBucket: "nytimes-dd655.appspot.com",
+  messagingSenderId: "281579209731",
+  appId: "1:281579209731:web:6e878a1414260a1128fa36"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+// Initialize Firebase Authentication and get a reference to the service
+export const firebaseAuth = getAuth(app);
+
+
+
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDUzKgCYz7SDzzllZrAAxDp4B1HW6pzJAE",
+//   authDomain: "nytime-c3acf.firebaseapp.com",
+//   projectId: "nytime-c3acf",
+//   storageBucket: "nytime-c3acf.appspot.com",
+//   messagingSenderId: "797982734923",
+//   appId: "1:797982734923:web:ddee69df20c29985826bce"
+// };
---
+++ js/firestore.js
@@ -0,0 +1,29 @@
+// import { initializeApp } from "firebase/app";
+// import { getFirestore } from "firebase/firestore";
+
+// // TODO: Replace the following with your app's Firebase project configuration
+// // See: https://support.google.com/firebase/answer/7015592
+// const firebaseConfig = {
+//     FIREBASE_CONFIGURATION
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+
+
+// // Initialize Cloud Firestore and get a reference to the service
+// const db = getFirestore(app);
+
+
+// import { collection, addDoc } from "firebase/firestore";
+
+// try {
+//   const docRef = await addDoc(collection(db, "users"), {
+//     first: "Ada",
+//     last: "Lovelace",
+//     born: 1815
+//   });
+//   console.log("Document written with ID: ", docRef.id);
+// } catch (e) {
+//   console.error("Error adding document: ", e);
+// }
